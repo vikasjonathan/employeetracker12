6e1d8847c3b8413e1ad58d9160dcc41218546edf
Vikas David Jonathan <vikas.jonathan@gmail.com>
2024-03-06 16:32:32 -0600

    Added code for the connections.js

---
+++ config/connection.js
@@ -0,0 +1,16 @@
+const mysql = require("mysql2");
+require("dotenv").config();
+
+const connection = mysql.createConnection({
+    host:"localhost",
+    port:"3306",
+    user: process.env.DB_USER,
+    password: process.env.DB_PASSWORD,
+    database: process.env.DB_NAME
+});
+
+connection.connect(function (err) {
+    if (err) throw err
+})
+
+module.exports = connection;
